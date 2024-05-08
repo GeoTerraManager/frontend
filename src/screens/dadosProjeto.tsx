52cdaa8ecd34242aab0b2fbe59ee36dac2b9812c
biamontanini <montanini249@gmail.com>
2024-05-07 23:39:34 -0300

    finalizado tela projeto

--- src/screens/dadosProjeto.tsx
+++ src/screens/dadosProjeto.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { useAuth } from "../context/AuthContext";
 import MainNavbar from "../components/navbar/navbar";
 import NaoAutorizado from "../components/Errors/NãoAutorizado";
@@ -7,26 +7,36 @@ import Title from "../components/Title/title";
 import GraficoRecorrencia from "../components/dadosprojeto/graficoRecorrencia";
 import Equipe from "../components/dadosprojeto/equipe";
 import Poligonos from "../components/dadosprojeto/poligonos";
+import SubTitle from "../components/Title/subTitle";
+import { DadosInterprete, DadosRevisores } from "../components/dadosprojeto/dadosCargo";
 
 const DadosProjeto = () => {
   const { loading, authorized } = useAuth();
 
-  return loading ? (
-    <p>Loading...</p>
-  ) : authorized ? (
+  // return loading ? (
+  //   <p>Loading...</p>
+  // ) : authorized ? (
+  return (
     <Grid>
       <MainNavbar />
 
       <Title styles={{ marginTop: 100 }}>Nome Projeto</Title>
-      <Grid container item justifyContent="space-around">
+      <Grid container item justifyContent="space-around" >
         <Equipe />
         <Poligonos />
         <GraficoStatus />
         <GraficoRecorrencia />
       </Grid>
+      <Grid container item justifyContent="space-around" style={{marginBottom: 30}}>
+        <DadosRevisores/>
+        <DadosInterprete/>
+      </Grid>
+      
+      
     </Grid>
-  ) : (
-    <NaoAutorizado />
+    // ) : (
+    //   <NaoAutorizado />
+    // );
   );
 };
 
